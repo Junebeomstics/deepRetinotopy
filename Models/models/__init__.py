f0a1c3f68457a891533d0b688b601f71ab9c2fab
Junbeom <junb@psyc-a87984.psy.utexas.edu>
2025-12-22 14:49:35 -0600

    remove FNO related codes

--- Models/models/__init__.py
+++ Models/models/__init__.py
@@ -5,16 +5,11 @@ from models.baseline import deepRetinotopy_Baseline
 from models.transolver_optionA import deepRetinotopy_OptionA
 from models.transolver_optionB import deepRetinotopy_OptionB
 from models.transolver_optionC import deepRetinotopy_OptionC
-from models.fno import FNOForRetinotopy, SimpleFNO, GraphToGridFNO, FNO1d
 
 __all__ = [
     'deepRetinotopy_Baseline',
     'deepRetinotopy_OptionA',
     'deepRetinotopy_OptionB',
     'deepRetinotopy_OptionC',
-    'FNOForRetinotopy',
-    'SimpleFNO',
-    'GraphToGridFNO',
-    'FNO1d',
 ]
 
